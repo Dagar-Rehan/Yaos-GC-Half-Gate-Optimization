80e5f293921702ae31beb83516db11c8e2e017ee
Dagar Rehan <dagar.rehan@gmail.com>
2024-05-03 15:56:42 -0400

    Now the evaulator can measure its run time.

--- src/pkg/evaluator.cxx
+++ src/pkg/evaluator.cxx
@@ -76,6 +76,14 @@ std::string EvaluatorClient::run(std::vector<int> input) {
   // Key exchange
   auto keys = this->HandleKeyExchange();
 
+  //to measure run time performance
+  using std::chrono::high_resolution_clock;
+  using std::chrono::duration_cast;
+  using std::chrono::duration;
+  using std::chrono::milliseconds;
+  auto t1 = high_resolution_clock::now();
+
+
   // TODO: implement me!
   std::pair<std::vector<unsigned char>, bool> intermediate;
 
@@ -192,6 +200,11 @@ std::string EvaluatorClient::run(std::vector<int> input) {
   final_output_msg.deserialize(std::get<0>(intermediate));
 
   std::cout << final_output_msg.final_output << std::endl; 
+
+  //to measure the run time
+  auto t2 = high_resolution_clock::now();
+  auto ms_int = duration_cast<milliseconds>(t2 - t1);
+  std::cout << "Time taken - " << ms_int.count() << " ms" << std::endl;
 
   return final_output_msg.final_output;
 }
